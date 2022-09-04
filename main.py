6fbd42fd0cd87a6c1f02349e3806986d9e397858
ETERNAL-PROGRAM\Mohammedrizwan Memon <rukaiyamemon2220@gmail.com>
2022-09-05 01:21:56 +0530

    Updated main.py Have written some more comments to make code more understandable.

--- main.py
+++ main.py
@@ -9,15 +9,12 @@ class StringCalculator():
         try:
             x = int(numbers)
             return x
-
         # if more than one number is present in the string and separated by delimiter 'comma(,)'
         except:
             # For initializing the variable giving the first value as '0' or '1' in the last test case
             position = 2
-
             # If another delimiter is present in the form of '//[delimiter]\n[numbers separated by delimiters]'
             try:
-
                 # For fetching '0' or '1' present in the 0th position of numbers
                 if '//' in numbers:
                     if numbers.startswith('0'):
@@ -30,17 +27,15 @@ class StringCalculator():
                 y = numbers.index('\n')
                 delimiter = numbers[x + 2:y]
                 numbers = numbers[y + 1:]
-
             # if no delimiter is given, then default is ',
             except:
                 delimiter = ','
-                # adding all the alphabets in alphaList and getting the value corresponding to each alphabet using its index in alphaList
+            # adding all the alphabets in alphaList and getting the value corresponding to each alphabet using its index in alphaList
             alphaList = []
             for i in range(97, 123, 1):
                 alphaList.append(chr(i))
 
             # if '\n' is present in numbers, it will be replace with ','
-
             numbers = numbers.replace("\n", delimiter)
 
             # separating all the numbers or alphabets present in the string and creating a list from it
@@ -58,25 +53,28 @@ class StringCalculator():
                 # runs if the current element is alphabet and takes the index of that alphabet from alphaList and gets the value corresponding to that alphabet
                 except:
                     val = alphaList.index(val) + 1
+
+                # Appends the negative numbers in the negativesList to raise an exception after appending all the negative numbers.
                 if val < 0:
                     negativesList.append(str(val))
+
                 # allocates the int value corresponding to the character present at that index
                 numList[key] = val
 
+            # checks if any negative number is present in the numList
             if len(negativesList) == 0:
 
                 # For fetching only the odd indexes of numList
                 if position == 0:
-                    return sum([val for key, val in enumerate(numList) if (key + 1) % 2 != 0])
-
+                    return sum([val for key, val in enumerate(numList) if (key+1) % 2 != 0])
                 # For fetching only the even indexes of numList
                 elif position == 1:
-                    return sum([val for key, val in enumerate(numList) if (key + 1) % 2 == 0])
-
+                    return sum([val for key, val in enumerate(numList) if (key+1) % 2 == 0])
                 # For fetching all indexes of numList
                 else:
                     return sum(numList)
+
+            # Raises the exception for negative numbers
             else:
                 negativesAll = ', '.join(negativesList)
                 raise Exception("\"Negatives not allowed\" - [" + negativesAll + "]")
-
