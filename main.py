e018142ca9f9e1c2fa66fb57c4d7534c2b8930c0
ETERNAL-PROGRAM\Mohammedrizwan Memon <rukaiyamemon2220@gmail.com>
2022-09-04 22:00:17 +0530

    Updated main.py and mainTesting.py to handle the test case where two numbers seperated by comma are passed as a string to the argument of add function of the StringCalculator class.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-
 class StringCalculator():
     def add(self, numbers=""):
         if len(numbers) == 0:
             return 0
-
-        else:
+        elif ',' not in numbers:
             return int(numbers)
+        else:
+            x, y = numbers.split(',')
+            return int(x) + int(y)
--- mainTesting.py
+++ mainTesting.py
@@ -2,11 +2,14 @@ import unittest
 from main import StringCalculator
 class StringCalculatorTestCase(unittest.TestCase):
     stringCalc = StringCalculator()
-
     def test_empty_string(self):
         result = self.stringCalc.add("")
         result1 = self.stringCalc.add("2")
         result2 = self.stringCalc.add("22")
+        result3 = self.stringCalc.add("2,5")
+        result4 = self.stringCalc.add("21,6")
         self.assertEqual(result, 0)
         self.assertEqual(result1, 2)
         self.assertEqual(result2, 22)
+        self.assertEqual(result3, 7)
+        self.assertEqual(result4, 27)
