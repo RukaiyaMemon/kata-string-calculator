45db86f9827a21e9132adc38449e73ebeed21e85
ETERNAL-PROGRAM\Mohammedrizwan Memon <rukaiyamemon2220@gmail.com>
2022-09-04 21:52:05 +0530

    Updated main.py file to add the code for only the single number passed as an argument to the add function of class StringCalculator.
    Updated mainTesting.py file to test the code for all the previous conditions alongwith the new condition of passing a single number as an argument to the add function of class StringCalculator.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-class StringCalculator():
 
+class StringCalculator():
     def add(self, numbers=""):
         if len(numbers) == 0:
             return 0
+
+        else:
+            return int(numbers)
--- mainTesting.py
+++ mainTesting.py
@@ -1,10 +1,12 @@
 import unittest
 from main import StringCalculator
-
-
 class StringCalculatorTestCase(unittest.TestCase):
     stringCalc = StringCalculator()
 
     def test_empty_string(self):
         result = self.stringCalc.add("")
+        result1 = self.stringCalc.add("2")
+        result2 = self.stringCalc.add("22")
         self.assertEqual(result, 0)
+        self.assertEqual(result1, 2)
+        self.assertEqual(result2, 22)
