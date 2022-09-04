f3b73736b766159333dffafa836de2eeb33eeee5
ETERNAL-PROGRAM\Mohammedrizwan Memon <rukaiyamemon2220@gmail.com>
2022-09-04 22:31:15 +0530

    Updated main.py and mainTesting.py files to follow all the previous test cases and a new test case to ignore the number greater than 1000 by changing such values to '0' passed in the string as an argument to the add function of StringCalculator class.

--- main.py
+++ main.py
@@ -7,21 +7,31 @@ class StringCalculator():
         # if string is empty
         if len(numbers) == 0:
             return 0
+
         # if there is only number present in the string
         elif ',' not in numbers:
             return int(numbers)
-        # if more than one number is present in the string and seperated by delimeter 'comma(,)'
+        # if more than one number is present in the string and seperated by delimeter '(,)'
+
         else:
+
             # adding all the alphabets in alphaList and getting the value corresponding to each alphabet using its index in alphaList
             alphaList = []
             for i in range(97, 123, 1):
                 alphaList.append(chr(i))
+
             # separating all the numbers or alphabets present in the string and creating a list from it
             numList = numbers.split(',')
             for key, val in enumerate(numList):
+
                 # runs if current element is number and converts the character of string into integer
                 try:
                     val = int(val)
+
+                    # if value is greater than 1000 then it should be ignored, so it is set to 0
+                    if val > 1000:
+                        val = 0
+
                 # runs if the current element is alphabet and takes the index of that alphabet from alphaList and gets the value corresponding to that alphabet
                 except:
                     val = alphaList.index(val) + 1
@@ -29,9 +39,8 @@ class StringCalculator():
                 if val < 0:
                     negativesList.append(str(val))
 
-                # allocates the int value corresponding to the character present at that particular index
+                # allocates the int value corresponding to the character present at that index
                 numList[key] = val
-
             if len(negativesList) == 0:
                 return sum(numList)
             else:
--- mainTesting.py
+++ mainTesting.py
@@ -2,22 +2,33 @@ import unittest
 from main import StringCalculator
 class StringCalculatorTestCase(unittest.TestCase):
     stringCalc = StringCalculator()
+
     def test_empty_string(self):
+
         result = self.stringCalc.add("")
-        result1 = self.stringCalc.add("2")
-        result2 = self.stringCalc.add("22")
-        result3 = self.stringCalc.add("2,5")
-        result4 = self.stringCalc.add("21,6")
-        result5 = self.stringCalc.add("2,5,6,7,8")
-        result6 = self.stringCalc.add("21, 06, 7, 100, 050")
-        result7 = self.stringCalc.add("1,2,a,c")
-        result8 = self.stringCalc.add("22,x,6,b")
         self.assertEqual(result, 0)
+
+        result1 = self.stringCalc.add("2")
         self.assertEqual(result1, 2)
+        result2 = self.stringCalc.add("22")
         self.assertEqual(result2, 22)
+
+        result3 = self.stringCalc.add("2,5")
         self.assertEqual(result3, 7)
+        result4 = self.stringCalc.add("21,6")
         self.assertEqual(result4, 27)
+
+        result5 = self.stringCalc.add("2,5,6,7,8")
         self.assertEqual(result5, 28)
+        result6 = self.stringCalc.add("21, 06, 7, 100, 050")
         self.assertEqual(result6, 184)
+
+        result7 = self.stringCalc.add("1,2,a,c")
         self.assertEqual(result7, 7)
+        result8 = self.stringCalc.add("22,x,6,b")
         self.assertEqual(result8, 54)
+
+        result9 = self.stringCalc.add("1,999, 1000, 5, 1010")
+        self.assertEqual(result9, 2005)
+        result10 = self.stringCalc.add("1,999, 1000, 1001, 5")
+        self.assertEqual(result10, 2005)
